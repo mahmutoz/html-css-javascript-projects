919eda4ca9f16296a4a78a4c525141acef67e3c7
mahmutoz <mahmutoz8197@gmail.com>
2021-06-07 11:10:04 +0300

    localstorage add and read but not deleted

--- 6-product-add-delete/final/script.js
+++ 6-product-add-delete/final/script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+localStorageRead();
+
 function UI() {}
 
 function Products(productName, productModel, productPrice) {
@@ -19,7 +21,6 @@ document.getElementById('product-add').addEventListener('submit', function (e) {
     if (productName === '' || productModel === '' || productPrice === '') {
         ui.productMessages("Please do not leave any blank spaces.", "error");
     } else {
-
         ui.productAdd(product);
         ui.productMessages('The product has been successfully added.', 'success');
     }
@@ -33,15 +34,55 @@ document.querySelector('.table').addEventListener('click', function (e) {
     e.preventDefault();
 });
 
+function localStorageOperations(){
+    let lName, lModel, lPrice;
+
+    if(localStorage.getItem('lName') === null && localStorage.getItem('lModel') === null && localStorage.getItem('lPrice') === null){
+        lName = [];
+        lModel = [];
+        lPrice = [];
+    } else {
+        lName = JSON.parse(localStorage.getItem('lName'));
+        lModel = JSON.parse(localStorage.getItem('lModel'));
+        lPrice = JSON.parse(localStorage.getItem('lPrice'));
+    }
+    return [lName,lModel, lPrice];
+}
+
+function localStorageAdd(pName, pModel, pPrice){
+    const [lName, lModel, lPrice] = localStorageOperations();
+    lName.push(pName);
+    lModel.push(pModel);
+    lPrice.push(pPrice);
+
+    localStorage.setItem('lName',JSON.stringify(lName));
+    localStorage.setItem('lModel',JSON.stringify(lModel));
+    localStorage.setItem('lPrice',JSON.stringify(lPrice));
+}
+
+function localStorageRead(){
+    const tableBody = document.querySelector('.table-body');
+    const tableEl = document.querySelector('.table');
+    let [lName, lModel, lPrice] = localStorageOperations();
+
+    for (let i = 0; i < lName.length; i++) {
+        const tr = document.createElement('tr');
+        tr.innerHTML = `<td>${lName[i]}</td><td>${lModel[i]}</td><td>${lPrice[i]}</td><td class="del">X</td>`;
+        console.log(tr);
+        tableBody.appendChild(tr);
+    }
+    tableEl.style.display = 'table';
+}
+
 UI.prototype.productAdd = function (product) {
     const tableBody = document.querySelector('.table-body');
     const tableEl = document.querySelector('.table');
     const tr = document.createElement('tr');
 
     tr.innerHTML = `<td>${product.productName}</td><td>${product.productModel}</td><td>${product.productPrice}</td><td class="del">X</td>`;
-
     tableBody.appendChild(tr);
     tableEl.style.display = 'table';
+    localStorageAdd(product.productName, product.productModel, product.productPrice);
 }
 
 UI.prototype.productMessages = function (message, className) {
@@ -65,4 +106,12 @@ UI.prototype.clearForm = function (product) {
 
 UI.prototype.productDelete = function (target) {
     (target.className === 'del') ? target.parentElement.remove(): "";
+    localStorageDelete(target.parentElement);
+}
+
+function localStorageDelete(item){
+    const [lName, lModel, lPrice] = localStorageOperations();
+
+
+    //item.children[i].innerText
 }
